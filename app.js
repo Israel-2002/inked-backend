1823e6831a79210a18a1a0480a0057d3129d8d6c
izzy <israelrankin@gmail.com>
2025-10-10 10:14:34 +0000

    added cors and route prefix

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const { config } = require("dotenv");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(authRoutes);
+app.use("/auth", authRoutes);
 
 mongoose
     .connect(process.env.MONGODB_URI)
