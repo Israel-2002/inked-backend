10e32df38d0d3a16e9d78195585c3bf5c4429e4a
izzy <israelrankin@gmail.com>
2025-10-10 10:17:07 +0000

    added id property and sanitized user object

--- utils/sanitize-user.js
+++ utils/sanitize-user.js
@@ -1,7 +1,10 @@
 exports.sanitizeUser = (userDoc) => {
     const user = userDoc.toObject()
+    user.id = user._id
 
-    delete user.password
+    delete user.__v
+    delete user._id
+    delete user.personal_info.password
     delete user.refresh_token
 
     return user
